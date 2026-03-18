0bc93cec9bf4e66eb3a5df22b34f2bd429fd3d84
Borys Czajkowski <s33205@pjwstk.edu.pl>
2026-03-18 09:26:07 +0100

    Dodano klase Person i dziedzicz. po niej Student i Employee

---
+++ ConsoleApp1/ConsoleApp1/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1;
+
+public class Employee : Person
+{
+    private double Salary { get; set;}
+
+    public Employee(double salary, string name, int age) : base(name, age)
+    {
+        Salary = salary;
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1;
+
+public class Student : Person
+{
+    public string Index { get; set; }
+    public Student (string index, string name, int age) : base(name, age)
+    {
+        Index = index;
+    }
+}
