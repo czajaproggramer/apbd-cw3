0104a5d702796fbbae1c35e61098f0c217f489c2
borisihta <kontak@przestrona.pl>
2026-03-20 22:45:30 +0100

    Zmodyfikowano klase record oraz w Program przetestowano podstawowy scenariusz programu

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// uzyt. x wypozycza od pracownika y sprzet z
+
+using ConsoleApp1;
+
+Student stud1 = new Student("s33205", "Antek", 18);
+Employee emp1 = new Employee(4800.5, "Zenon", 25);
+Laptop dev1 = new Laptop("Macbook air", "Apple", 4800, "MacOS", 2021);
+
+Record rec1 = new Record(DateTime.Parse("2026-02-02"), 7, emp1, stud1, dev1,0, 0);
+Console.WriteLine(rec1);
--- ConsoleApp1/ConsoleApp1/Record.cs
+++ ConsoleApp1/ConsoleApp1/Record.cs
@@ -6,16 +6,24 @@ public class Record
     public int LeaseDays { get; set; }
     public Employee Worker { get; }
     public Student User { get; }
+    public Device LeaseObject { get; }
     public float BasePrice { get; }
     public float LateFees { get; set; }
 
-    public Record(DateTime leaseDate, int leaseDays, Employee worker, Student user, float basePrice, float lateFees)
+    public Record(DateTime leaseDate, int leaseDays, Employee worker, Student user, Device leaseObject, float basePrice, float lateFees)
     {
         LeaseDate = leaseDate;
         LeaseDays = leaseDays;
         Worker = worker;
         User = user;
+        LeaseObject = leaseObject;
         BasePrice = basePrice;
         LateFees = lateFees;
+    }
+
+    public override string ToString()
+    {
+        return "Dnia " + LeaseDate.ToShortDateString() + " student " + User.Name + " (" + User.Index +
+               ") wypoyczył " + LeaseObject.Name + " na okres " + LeaseDays + "dni.";
     }
 }
